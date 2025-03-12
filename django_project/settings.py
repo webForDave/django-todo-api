5ba72e0c253bf4aa535505826e2e53dc0d47700c
David Akinola <dakinola54@gmail.com>
2025-03-12 12:31:00 +0100

    CORS

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # external
     "rest_framework",
+    "corsheaders"
     # internal
     "todos.apps.TodosConfig"
 ]
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -131,3 +133,8 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticatedOrReadonly"
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "https://localhost:8000" # backend port
+    "https://localhost:3000" # frontend port
+]
