aac80ecefeaca3003cb095568b89cd8d1d7e5caa
David Akinola <dakinola54@gmail.com>
2025-03-12 12:03:24 +0100

    added urls, and views for endpoints

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # external
+    "rest_framework",
     # internal
     "todos.apps.TodosConfig"
 ]
