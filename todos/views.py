8035a8880d6367ac8528de83f97103f7b6f247ac
David Akinola <dakinola54@gmail.com>
2025-03-12 12:16:07 +0100

    overide endpoints permissions

--- todos/views.py
+++ todos/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Todo
 from .serializers import TodoSerializer
+from rest_framework.permissions import AllowAny
 
 class ListTodo(generics.ListAPIView):
+    permission_classes = [AllowAny]
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
 class DetailTodo(generics.RetrieveAPIView):
+    permission_classes = [AllowAny]
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
